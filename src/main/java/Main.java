1643b2f9ec148042ed30cf4e644cea9d6a1e78cf
Ivar Högblom <ivar.hogblom@hotmail.com>
2025-03-14 12:19:34 +0100

    Fifth Commit. Logic works, comitting before breaking out code in smaller pieces.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,23 +2,24 @@ import java.util.Locale;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        /*
-        TODO 1 Menu Display: The application should display a menu to the user with options to convert between
-          different currencies or exit the program.
-        TODO 2 User Input Handling: The application should capture user input for menu choices and currency
-         amounts.
-         TODO 3 Currency Conversion: The application should accurately convert amounts between SEK, USD, and
-          Euro using predefined exchange rates.
-          TODO 4 Formatted Output: The application should display conversion results in a formatted manner
-           along with the current date and time.
-           TODO 5 Data Validation: The application should handle invalid inputs gracefully (e.g., non-numeric
-            values, negative amounts).
 
-        Code Structure: The application should be well-structured using classes and methods for clarity and
-        maintainability.
-         */
+public class Main {
+    /*
+TODO 1 Menu Display: The application should display a menu to the user with options to convert between
+  different currencies or exit the program.
+TODO 2 User Input Handling: The application should capture user input for menu choices and currency
+ amounts.
+ TODO 3 Currency Conversion: The application should accurately convert amounts between SEK, USD, and
+  Euro using predefined exchange rates.
+  TODO 4 Formatted Output: The application should display conversion results in a formatted manner
+   along with the current date and time.
+   TODO 5 Data Validation: The application should handle invalid inputs gracefully (e.g., non-numeric
+    values, negative amounts).
+
+Code Structure: The application should be well-structured using classes and methods for clarity and
+maintainability.
+ */
+    public static void main(String[] args) {
         Locale.setDefault(Locale.US);
 
         final int EXITNUMBER = 0;
